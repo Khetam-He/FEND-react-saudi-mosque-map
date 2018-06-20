756dcd3495aad212a99442eaff629d0a9dd7cf28
Khetam-He <toty.f.h@hotmail.com>
2018-06-20 16:17:35 +0300

    required changes
    updating some required changes and appling some suggistions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 }
 from 'react-helmet'
 import './App.css';
+import Markers from'./markers';
 import Map from './Map'
 import Sidebar from "./Sidebar";
 
@@ -20,48 +21,7 @@ class App extends Component {
             map: '',
             info: '',
             //includes all the markers info (latitudes ,longitude , name , website)
-            markers: [{
-                    lat: 21.423323,
-                    long: 39.826399,
-                    name: 'Al-Masjid Al Haram _ Macca',
-                    website: 'https://en.wikipedia.org/wiki/Great_Mosque_of_Mecca'
-                }, {
-                    lat: 21.381667,
-                    long: 39.874444,
-                    name: 'Masjid Al-Rajhi _ Macca',
-                    website: 'https://www.beautifulmosque.com/Al-Rajhi-Mosque-in-Mecca-Saudi-Arabia'
-                }, {
-                    lat: 24.467191,
-                    long: 39.611099,
-                    name: 'Al-Masjid al-Nabawi _Medina',
-                    website: 'https://en.wikipedia.org/wiki/Al-Masjid_an-Nabawi'
-                }, {
-                    lat: 24.4367515863,
-                    long: 39.616997532,
-                    name: 'Quba Mosque _ Medina',
-                    website: 'https://en.wikipedia.org/wiki/Quba_Mosque'
-
-                }, {
-                    lat: 24.706997172,
-                    long: 46.721163782,
-                    name: 'Imam Turki Bin Abdullah Grand Mosque _ Riyadh',
-                    website: 'https://en.wikipedia.org/wiki/Imam_Turki_bin_Abdullah_Mosque'
-
-                },
-                {
-                    lat: 21.6486985,
-                    long: 39.1008551,
-                    name: 'Al-Rahma Mosque _ Jeddah',
-                    website: 'https://www.tripadvisor.com/ShowUserReviews-g295419-d319513-r328313591-Floating_Mosque-Jeddah_Makkah_Province.html'
-                }, {
-                    lat: 25.8449509,
-                    long: 44.1979471,
-                    name: 'Khubaib Bin Addey Mosque _ Qaseem',
-                    website: 'https://en.wikipedia.org/wiki/Khubayb_ibn_Adiy'
-                }
-
-
-            ],
+            markers: Markers,
             virtualMarkers: []
         };
 
@@ -167,13 +127,14 @@ class App extends Component {
                 }
                 response.json().then(function(data) {
                     var place = data.response.venues[0];
+                      var web=self.marker.website;
 
 
                     var info =
                         "<div id='marker'>" +
                         "<h2>" + self.marker.title + "</h2>" +
                         "<p><b>Address:</b> " + place.location.address + ", " + place.location.city + "</p>" +
-                        "<p><b>website:</b> " +"<a target= \"_blank\" href=>"+self.marker.website+"</a>"+
+                        `<p><b>website:</b><a target= \"_blank\" href= ${web} > ${web}</a></p>`+
                         "</div>";
                     self.setContent(info);
                 });
---
+++ src/markers.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+var Markers=
+[{
+                    lat: 21.423323,
+                    long: 39.826399,
+                    name: 'Al-Masjid Al Haram _ Macca',
+                    website: 'https://en.wikipedia.org/wiki/Great_Mosque_of_Mecca'
+                }, {
+                    lat: 21.381667,
+                    long: 39.874444,
+                    name: 'Masjid Al-Rajhi _ Macca',
+                    website: 'https://www.beautifulmosque.com/Al-Rajhi-Mosque-in-Mecca-Saudi-Arabia'
+                }, {
+                    lat: 24.467191,
+                    long: 39.611099,
+                    name: 'Al-Masjid al-Nabawi _Medina',
+                    website: 'https://en.wikipedia.org/wiki/Al-Masjid_an-Nabawi'
+                }, {
+                    lat: 24.4367515863,
+                    long: 39.616997532,
+                    name: 'Quba Mosque _ Medina',
+                    website: 'https://en.wikipedia.org/wiki/Quba_Mosque'
+
+                }, {
+                    lat: 24.706997172,
+                    long: 46.721163782,
+                    name: 'Imam Turki Bin Abdullah Grand Mosque _ Riyadh',
+                    website: 'https://en.wikipedia.org/wiki/Imam_Turki_bin_Abdullah_Mosque'
+
+                },
+                {
+                    lat: 21.6486985,
+                    long: 39.1008551,
+                    name: 'Al-Rahma Mosque _ Jeddah',
+                    website: 'https://www.tripadvisor.com/ShowUserReviews-g295419-d319513-r328313591-Floating_Mosque-Jeddah_Makkah_Province.html'
+                }, {
+                    lat: 25.8449509,
+                    long: 44.1979471,
+                    name: 'Khubaib Bin Addey Mosque _ Qaseem',
+                    website: 'https://en.wikipedia.org/wiki/Khubayb_ibn_Adiy'
+                }
+
+
+            ];
+
+
+export default Markers;
